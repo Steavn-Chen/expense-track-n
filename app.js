e37e0779b05c3aef799b9d9e359dbbd97e80ac0e
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-31 15:49:03 +0800

    安裝express-handlebars並設定

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send('123')
+  res.render('index')
 })
 
 app.listen(port, () => {
