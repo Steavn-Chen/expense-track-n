c80200bd7f46ab4afd9551e91927a24e78c7be93
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-31 16:16:06 +0800

    設置靜態檔案資料夾publice，並設置favicon

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
