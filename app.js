df1e9ed42471b9dd5d4ed795764badac10287ea6
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-02 00:58:33 +0800

    安裝mongoose並連線到資料庫

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const recordList = require('./records.json')
-const categoryList = require("./categories.json");
+const categoryList = require('./categories.json')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/expense-trackerss', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb is error !')
+})
+
+db.once('open', () => {
+  console.log('mongodb is connected.')
+})
 
 const app = express()
 const port = 3000
@@ -15,8 +30,8 @@ app.get('/', (req, res) => {
   res.render('index', { records: recordList })
 })
 app.get("/new", (req, res) => {
-  res.render("new");
-});
+  res.render("new")
+})
 
 app.listen(port, () => {
   console.log(`Expense-tracker web is running on http://localhost:${port}`)
