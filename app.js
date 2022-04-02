f5db36182aa80d3cb04e84bcd0cce2c7422981a7
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-02 19:24:21 +0800

    新增 (新增一筆記錄路由)

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const Record = require('./models/record.js')
 // const recordList = require('./models/seeds/records.json')
 // const categoryList = require('./models/seeds/categories.json')
+const Category = require('./models/category.js')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/expense-trackerss', {
   useNewUrlParser: true, 
@@ -22,6 +24,8 @@ db.once('open', () => {
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
 
@@ -31,12 +35,59 @@ app.get('/', (req, res) => {
   return Record.find()
     .lean()
     .then(records => {
-      res.render("index", { records: records })
+      res.render('index', { records: records })
     })
     .catch(err => console.error(err))
 })
-app.get("/new", (req, res) => {
-  res.render("new")
+app.get('/new', (req, res) => {
+  return Category.find()
+    .lean()
+    .then(categories => {
+      res.render('new', { categories: categories }) 
+    })
+    .catch(err => console.error(err))
+})
+app.post('/new', (req, res) => {
+
+  // 第一種搜尋方式 
+  // return Category.findOne({ category: req.body.category})
+  //  第二種搜尋方式
+  return Category.aggregate([
+    {
+      $match: { category: req.body.category } 
+    }, 
+    {
+      $project: { _id: 0, __v: 0, category_en: 0, category:0 }
+    },
+    {
+      $project: { categoryIcon: '$icon' }
+    }
+  ])
+    .then((category) => {
+      const icon = category[0].categoryIcon
+      // 第一種寫法
+      return Record.create({...req.body, icon })
+        .then((record) => {
+          res.render('new')
+        })
+        .catch((err) => console.error(err))
+      // 第二種寫法
+      // const newBody = Object.assign(req.body, { icon })
+      // return Record.create(newBody)
+      //   .then(() => res.render('new'))
+      //   .catch(err => console.error(err))
+      // 第三種寫法
+      // return Record.create({
+      //   name: req.body.name,
+      //   date: req.body.date,
+      //   category: req.body.category,
+      //   amount: req.body.amount,
+      //   icon: icon,
+      // })
+      //   .then(() => res.render('new'))
+      //   .catch((err) => console.error(err))
+    })
+    .catch((err) => console.error(err))
 })
 
 app.listen(port, () => {
