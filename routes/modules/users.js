503ee96db16114ab08bf88fdf513e5768dcdb307
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-20 13:57:17 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { append } = require('express/lib/response')
 const router = express.Router()
+const User = require('../../models/user.js')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -11,6 +11,37 @@ router.post('/login', (req, res) => {})
 router.get('/register', (req, res) => {
   res.render('register')
 })
-router.post('/register', (req, res) => {})
+router.post('/register', (req, res) => {
+  const { email, name, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log('此帳號己被註冊.')
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword,
+        })
+      }
+      // if (password !== confirmPassword) {
+      //   console.log('密碼與確認密碼不相符 !')
+      //   return res.render('register', {
+      //     name,
+      //     email,
+      //     password,
+      //     confirmPassword,
+      //   })
+      // }
+      return User.create({
+        name,
+        email,
+        password,
+      })
+        .then((user) => res.redirect('/'))
+        .catch((err) => console.log(err))
+    })
+    .catch((err) => console.log(err))
+})
 
 module.exports = router
